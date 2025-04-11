46111421bef763e5452ab35fdfcfccad22f87d19
BejenaruIoanMatei <bejematei@gmail.com>
2025-04-11 16:56:03 +0300

    2. Templates for home and about page

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['localhost', '127.0.0.1'])
 # Application definition
 
 INSTALLED_APPS = [
+    'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

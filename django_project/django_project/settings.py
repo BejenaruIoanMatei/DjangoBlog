ebe554a83de593de1fc878f3a1dd95f86b94a616
BejenaruIoanMatei <bejematei@gmail.com>
2025-04-28 19:05:04 +0300

    12: Friend System: Models + Admin register

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['localhost', '127.0.0.1'])
 # Application definition
 
 INSTALLED_APPS = [
+    'friend.apps.FriendConfig',
     'users.apps.UsersConfig',
     'blog.apps.BlogConfig',
     'crispy_forms',
